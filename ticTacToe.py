3ce903e355d6840b54987eedc7ea676de6f3389e
Patrick <patricksbarnard@gmail.com>
2023-07-05 16:07:24 +0100

    test

---
+++ ticTacToe.py
@@ -0,0 +1,7 @@
+print("Welcome to the two player tic tac toe!")
+player1 = input("What is your name player 1?\n")
+player2 = input("What is your name player 2?\n")
+print(f"{player1} the mighty O vs {player2} the valiant X! Let the battle begin!")
+print (" a | b | c \n-----------\n d | e | f \n-----------\n g | h | i ")
+
+hello world
