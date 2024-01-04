e2ee689f6393e3044d86af4eaa0705db1cf5fcc6
patricksbarnard <patricksbarnard@gmail.com>
2024-01-04 17:45:34 +0000

    playable with some bugs

--- ticTacToe.py
+++ ticTacToe.py
@@ -17,5 +17,38 @@ def check_winner(board, player):
 def is_board_full(board):
      return all(board[i][j] != ' ' for i in range(3) for j in range(3))
 
+def play_game():
+     board = [[' ' for _ in range(3)] for _ in range(3)]
+     current_player = 'X'
+     
+     while True:
+        print_board(board)
 
+        #player move
+        row = int(input("picks a row (0,1,2):"))
+        col = int(input("pick a column 90,1,2):"))
 
+        #game course
+        #empty cell?
+        if board[row][col] == ' ':
+             board[row][col] = current_player
+
+             #winner?
+             if check_winner(board, current_player):
+                  print(board)
+                  print(f"player {current_player} wins")
+                  break
+             
+             #tie?
+             if is_board_full(board):
+                  print(board)
+                  print("tie")
+                  break
+             
+             #change player
+             current_player = 'O' if current_player =='X' else 'X'
+        else:
+             print("pick a diffrent cell")
+
+if __name__ == "__main__":
+    play_game()
