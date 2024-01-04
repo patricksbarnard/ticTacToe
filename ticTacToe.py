7087abfa8a3c97f3531aacac654d703aab9131ba
patricksbarnard <patricksbarnard@gmail.com>
2024-01-04 17:08:29 +0000

    prints the board

--- ticTacToe.py
+++ ticTacToe.py
@@ -1,7 +1,6 @@
-print("Welcome to the two player tic tac toe!")
-player1 = input("What is your name player 1?\n")
-player2 = input("What is your name player 2?\n")
-print(f"{player1} the mighty O vs {player2} the valiant X! Let the battle begin!")
-print (" a | b | c \n-----------\n d | e | f \n-----------\n g | h | i ")
+def print_board(board):
+    # prints the board in current state of play
+        for row in board:
+                print(" | ".join(row))
+                print("-" * 9)
 
-prin("hello world")
