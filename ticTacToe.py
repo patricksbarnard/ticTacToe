f5b219ed264cefccf5f6a918a467330d7e7c0464
patricksbarnard <patricksbarnard@gmail.com>
2024-01-04 17:27:33 +0000

    chek for a winner

--- ticTacToe.py
+++ ticTacToe.py
@@ -4,3 +4,14 @@ def print_board(board):
                 print(" | ".join(row))
                 print("-" * 9)
 
+def check_winner(board, player):
+    #rows and columns
+    for i in range(3):
+         if all(board[i][j] == player for j in range(3)) or all(board[j][i] == player for j in range(3)):
+            return True 
+    #diagonals
+    if all(board[i][i] ==player for i in range(3)) or all(board[i][2-i] == player for i in range(3)):
+         return True
+    return False
+
+
